39f5df6358a162731f880a20e05ddee98606f74f
lutfur <lutfur@gain.media>
2023-06-14 13:40:40 +0600

    update code and bug fixing ::: MORE & PAYSLIP MODULE

--- lib/modules/starting/view/onboarding_screen.dart
+++ lib/modules/starting/view/onboarding_screen.dart
@@ -17,7 +17,6 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
-
   final List _onboardImage = [
     Images.calendar,
     Images.easy_leave,
